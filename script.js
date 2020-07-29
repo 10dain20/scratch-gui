ed5f84ba3c9f818daa252b7c73dae96f063ac1f4
10dain20 <10dain20@gmail.com>
2020-07-29 15:31:06 +0900

    edit script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const dfMessenger = document.querySelector('df-messenger');
-var marginsize = '500px'
+var marginsize = '520px'
 
 dfMessenger.addEventListener('mousemove', function (event) {
   	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
