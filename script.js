b1a5fbb630be6c0b9a769276105343987a4e2129
10dain20 <10dain20@gmail.com>
2020-07-29 14:47:36 +0900

    add new file

---
+++ script.js
@@ -0,0 +1,13 @@
+const dfMessenger = document.querySelector('df-messenger');
+var marginsize = '500px'
+
+dfMessenger.addEventListener('mousemove', function (event) {
+  	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+  	dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+
+});
+
+dfMessenger.addEventListener('df-response-received', function (event) {
+  	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+  	dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);	
+});
