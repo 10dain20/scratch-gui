b18063b3dd6588b77cafbb6007e3d833826cecdf
10dain20 <10dain20@gmail.com>
2020-08-03 18:43:37 +0900

    add message list wrapper

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const dfMessenger = document.querySelector('df-messenger');
 var marginsize = '520px'
 
 dfMessenger.addEventListener('dfMessengerLoaded', function (event) {
-  	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+	dfMessenger.shadowRoot.querySelector('message-list-wrapper minimized').setAttribute('style', 'margin-right: ' + marginsize);
   	dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
 
 });
