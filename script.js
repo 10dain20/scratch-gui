ae52b4be81a23161d06b3ad9e4dfb540e745ee73
10dain20 <10dain20@gmail.com>
2020-08-03 18:13:51 +0900

    use dfMessengerLoaded event listener

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const dfMessenger = document.querySelector('df-messenger');
 var marginsize = '520px'
 
-dfMessenger.addEventListener('mousemove', function (event) {
+dfMessenger.addEventListener('dfMessengerLoaded', function (event) {
   	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
   	dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
 
