7e8f193677b8c3fc4ecb997d03284c18fc51dbc3
10dain20 <10dain20@gmail.com>
2020-08-04 16:32:11 +0900

    use script.js to make backspace work

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const dfMessenger = document.querySelector('df-messenger');
 var marginsize = '520px'
 
 dfMessenger.addEventListener('dfMessengerLoaded', function (event) {
-	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
-	dfMessenger.shadowRoot.querySelector('message-list-wrapper minimized').setAttribute('style', 'margin-right: ' + marginsize);
-  	dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+	window.console.log(dfMessenger.style.display)
+	// dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+	// dfMessenger.shadowRoot.querySelector('message-list-wrapper minimized').setAttribute('style', 'margin-right: ' + marginsize);
+  	// dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
 
 });
 
-dfMessenger.addEventListener('df-response-received', function (event) {
-  	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
-  	dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);	
-});
+// if (dfMessenger.style.display !== 'none') {
+// 	window.console.log("??")
+// 	window.console.log(dfMessenger.style.display)
+// }
+
+const inputBox = document.querySelector('.input-box-wrapper')
+window.console.log("input box render test", inputBox)
+// dfMessenger.addEventListener('df-response-received', function (event) {
+//   	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+//   	dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);	
+// });
