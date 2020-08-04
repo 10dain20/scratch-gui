3d9dc15387449a444aca6dc7af730d94f012a551
10dain20 <10dain20@gmail.com>
2020-08-04 16:45:44 +0900

    attempt 2345452

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ const dfMessenger = document.querySelector('df-messenger');
 var marginsize = '520px'
 
 dfMessenger.addEventListener('dfMessengerLoaded', function (event) {
-	window.console.log(dfMessenger.style.display)
+	// document.removeEventListener('keydown');
+	// document.removeEventListener('keyup');
+	// console.log("event listener removed")
+	// window.console.log(dfMessenger.style.display)
 	// dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
 	// dfMessenger.shadowRoot.querySelector('message-list-wrapper minimized').setAttribute('style', 'margin-right: ' + marginsize);
-  	// dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
-
+	// dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+	
 });
 
-// if (dfMessenger.style.display !== 'none') {
-// 	window.console.log("??")
-// 	window.console.log(dfMessenger.style.display)
-// }
+document.removeEventListener('keydown');
+	document.removeEventListener('keyup');
+	console.log("event listener removed")
 
-const inputBox = document.querySelector('.input-box-wrapper')
-window.console.log("input box render test", inputBox)
+// if (dfMessenger.style.display !== 'none') {
+	// 	window.console.log("??")
+	// 	window.console.log(dfMessenger.style.display)
+	// }
+	
+
 // dfMessenger.addEventListener('df-response-received', function (event) {
 //   	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
 //   	dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);	
