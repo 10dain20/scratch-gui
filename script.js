cbcc54be459fa56fb206e0b574965fe0b1823c66
10dain20 <10dain20@gmail.com>
2020-08-05 16:44:38 +0900

    Revert changes

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
 const dfMessenger = document.querySelector('df-messenger');
 var marginsize = '520px'
 
-dfMessenger.addEventListener('dfMessengerLoaded', function (event) {
+dfMessenger.addEventListener('mousemove', function (event) {
 	// document.removeEventListener('keydown');
 	// document.removeEventListener('keyup');
 	// console.log("event listener removed")
 	// window.console.log(dfMessenger.style.display)
-	// dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
-	// dfMessenger.shadowRoot.querySelector('message-list-wrapper minimized').setAttribute('style', 'margin-right: ' + marginsize);
-	// dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
-	
+
+	const wrapper = document.querySelector('df-messenger').shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper')
+
+	wrapper.removeEventListener('keydown', 
+	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+	dfMessenger.shadowRoot.querySelector('message-list-wrapper minimized').setAttribute('style', 'margin-right: ' + marginsize);
+	dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper')
+	window.console.log(dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper'))
+
 });
 
-document.removeEventListener('keydown', this.handleKeydown);
-	document.removeEventListener('keyup', this.handleKeyUp);
-	console.log("event listener removed")
-
-// if (dfMessenger.style.display !== 'none') {
-	// 	window.console.log("??")
-	// 	window.console.log(dfMessenger.style.display)
-	// }
-	
 
 // dfMessenger.addEventListener('df-response-received', function (event) {
 //   	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
