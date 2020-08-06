d59812554da22b034e58140d971d9536ccd0f707
10dain20 <68581621+10dain20@users.noreply.github.com>
2020-08-06 16:33:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const dfMessenger = document.querySelector('df-messenger');
-var marginsize = '520px'
+// var marginsize = '520px'
 
-dfMessenger.addEventListener('mousemove', function (event) {
+dfMessenger.addEventListener('dfMessengerLoaded', function (event) {
 	// document.removeEventListener('keydown');
 	// document.removeEventListener('keyup');
 	// console.log("event listener removed")
 	// window.console.log(dfMessenger.style.display)
 
-	const wrapper = document.querySelector('df-messenger').shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper')
-
-	wrapper.removeEventListener('keydown', 
-	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
-	dfMessenger.shadowRoot.querySelector('message-list-wrapper minimized').setAttribute('style', 'margin-right: ' + marginsize);
-	dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper')
-	window.console.log(dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper'))
+	const box = document.querySelector('df-messenger').shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper').querySelector('input')
+	console.log(box);
+	function dialogDelete(event) {
+		if (event.keyCode == 8){
+			box.value = box.value.substring(0, box.value.length - 1);
+		}
+	}
+	box.addEventListener('keydown', dialogDelete);
+	//wrapper.removeEventListener('keydown', 
+	//dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+	//dfMessenger.shadowRoot.querySelector('message-list-wrapper minimized').setAttribute('style', 'margin-right: ' + marginsize);
+	//dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper')
+	//window.console.log(dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper'))
 
 });
 
