2e73f4cde993db41ddb4a5ee2132023364764d18
10dain20 <10dain20@gmail.com>
2020-08-06 18:31:57 +0900

    fix backspace issue

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 const dfMessenger = document.querySelector('df-messenger');
-// var marginsize = '520px'
-window.console.log(dfMessenger);
+var x = 0;
 
 dfMessenger.addEventListener('df-request-sent', function (event) {
-	// document.removeEventListener('keydown');
-	// document.removeEventListener('keyup');
-	// console.log("event listener removed")
-	// window.console.log(dfMessenger.style.display)
-
 	const box = dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper').querySelector('input')
-	window.console.log(box + " is loadeded");
 	function dialogDelete(e) {
 		if (e.keyCode == 8){
 			box.value = box.value.substring(0, box.value.length - 1);
 		}
 	}
-	box.removeEventListener('keydown', dialogDelete);
-	box.addEventListener('keydown', dialogDelete);
+	if (x == 0){
+		box.addEventListener('keydown', dialogDelete);
+		x++;
+	}
+});
+
+	// document.removeEventListener('keydown');
+	// document.removeEventListener('keyup');
+	// console.log("event listener removed")
+	// window.console.log(dfMessenger.style.display)
 	//wrapper.removeEventListener('keydown', 
 	//dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
 	//dfMessenger.shadowRoot.querySelector('message-list-wrapper minimized').setAttribute('style', 'margin-right: ' + marginsize);
 	//dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper')
 	//window.console.log(dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('df-messenger-user-input').shadowRoot.querySelector('.input-box-wrapper'))
-
-});
-
-
-// dfMessenger.addEventListener('df-response-received', function (event) {
-//   	dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
-//   	dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);	
-// });
+	// dfMessenger.addEventListener('df-response-received', function (event) {
+	// dfMessenger.shadowRoot.querySelector('.df-messenger-wrapper').setAttribute('style', 'margin-right: ' + marginsize);
+	//  dfMessenger.shadowRoot.querySelector('df-messenger-chat').shadowRoot.querySelector('.chat-wrapper').setAttribute('style', 'margin-right: ' + marginsize);	
+	// });
