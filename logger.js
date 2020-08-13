1ae6f9e641485d3fb5b78b99f4dca98c25f27592
10dain20 <10dain20@gmail.com>
2020-08-13 15:01:45 +0900

    editasdf

--- logger.js
+++ logger.js
@@ -1,9 +1,8 @@
-
 console.log('hello');
 
-//mouse tracking 
+//MOUSE LOGGER(track mousemove and click events)
 (function(){
-  var mouse_log=" ";
+  var mouse_log=" "
 
   $(document).mousemove(function(e) {
     mouse_log += $.now() + " ";
@@ -18,19 +17,47 @@ console.log('hello');
     mouse_log += e.pageY + " ";
     mouse_log += "mouseclick\n"; 
   });
-  console.log(mouse_log);
+
+  (function(){
+    var p = function(){
+      console.log(mouse_log);
+    };
+    window.setInterval(p,500)
+    p();
+  })();
 })();
 
+//KEYBOARD LOGGER(track mousemove and click events)
+(function(){
+  var keyb_log=" "
+  $(document).keydown(function(e) {
 
+    keyb_log+= $.now()+" ";
+    keyb_log+= e.keyCode + " ";
+    keyb_log+= "keydown\n";
+    window.console.log(keyb_log);
+   })
+})();
 
-//tutorial log
-
-// var tutorial_log = []
-// const tutorial_play=document.getElementByClassName('w-big-play-button');
+// //TUTORIAL LOGGER
+// var tutorial_log = " "
+// var tutorial_count=0
+// const tutorial_play=document.getElementByClassName('w-vulcan-v2-button');
+// console.log(tutorial_play)
 
 // tutorial_play.addEventListener("click", function(event){
-//   function playedTimeStamp(e) { 
-//     var tutorial_played1 = e.timeStamp;
-//     tutorial_log.push(tutorial_played1)
-
-// console.log(tutorial_log);
+//   function tutorial_logger(e) { 
+//     var tutorial_played = e.timeStamp;
+//     tutorial_count+=1;
+  
+//     tutorial_log.push("Played:",tutorial_played);
+//     tutorial_log.push("Count:",tutorial_count);
+//   }
+//   (function(){   
+//     var q = function(){
+//       console.log(tutorial_log);
+//     };
+//     window.setInterval(p,1000)
+//     q();
+//   }
+// });                                                      
