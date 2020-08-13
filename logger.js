b6fe23e0793db8a64581e864bead1b01ef6387c4
10dain20 <10dain20@gmail.com>
2020-08-13 17:53:56 +0900

    keyboard and click event logger

--- logger.js
+++ logger.js
@@ -1,9 +1,8 @@
-console.log('hello');
-
 //MOUSE LOGGER(track mousemove and click events)
 (function(){
   var mouse_log=" "
-
+  
+  
   $(document).mousemove(function(e) {
     mouse_log += $.now() + " ";
     mouse_log += e.pageX + " ";
@@ -17,7 +16,7 @@ console.log('hello');
     mouse_log += e.pageY + " ";
     mouse_log += "mouseclick\n"; 
   });
-
+  
   (function(){
     var p = function(){
       console.log(mouse_log);
@@ -42,22 +41,9 @@ console.log('hello');
 // //TUTORIAL LOGGER
 // var tutorial_log = " "
 // var tutorial_count=0
-// const tutorial_play=document.getElementByClassName('w-vulcan-v2-button');
-// console.log(tutorial_play)
-
-// tutorial_play.addEventListener("click", function(event){
-//   function tutorial_logger(e) { 
-//     var tutorial_played = e.timeStamp;
-//     tutorial_count+=1;
-  
-//     tutorial_log.push("Played:",tutorial_played);
-//     tutorial_log.push("Count:",tutorial_count);
-//   }
-//   (function(){   
-//     var q = function(){
-//       console.log(tutorial_log);
-//     };
-//     window.setInterval(p,1000)
-//     q();
-//   }
-// });                                                      
+// function detectButton(event){
+//   var clickedItem=event.target;
+//   if (clickedItem.tagname =='svg'){
+//     console.log(clickedItem.className)
+//   };
+// };                                              
