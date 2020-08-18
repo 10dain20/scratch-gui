dbd3ff3113b15c2d53753133e80ac080895a887d
10dain20 <10dain20@gmail.com>
2020-08-18 16:45:55 +0900

    onunload

--- src/containers/gui.jsx
+++ src/containers/gui.jsx
@@ -44,6 +44,26 @@ class GUI extends React.Component {
         setIsScratchDesktop(this.props.isScratchDesktop);
         this.props.onStorageInit(storage);
         this.props.onVmInit(this.props.vm);
+
+        window.onbeforeunload = function (event) {
+            var message = 'Important: Please click on \'Save\' button to leave this page.';
+            if (typeof event == 'undefined') {
+                event = window.event;
+            }
+            if (event) {
+                event.returnValue = message;
+            }
+            return message;
+        };
+        
+        $(function () {
+            $("a").not('#lnkLogOut').click(function () {
+                window.onbeforeunload = null;
+            });
+            $(".btn").click(function () {
+                window.onbeforeunload = null;
+        });
+        });
     }
     componentDidUpdate (prevProps) {
         if (this.props.projectId !== prevProps.projectId && this.props.projectId !== null) {
